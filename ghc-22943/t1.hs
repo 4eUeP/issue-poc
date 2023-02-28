744f71a8775f48b75857a2fd5abedb837aeb7217
mu <59917266+4eUeP@users.noreply.github.com>
2023-02-28 17:23:42 +0800

    updates

--- ghc-22943/t1.hs
+++ ghc-22943/t1.hs
@@ -9,6 +9,7 @@ main :: IO ()
 main =
   -- Async.withAsync f1 $ \a -> Async.wait a
   -- f2
+  -- f4
   f3
 
 f1 :: IO ()
@@ -26,3 +27,8 @@ f3 =
     a1 <- Async.async f1
     Async.link2Only (const True) a a1
     Async.wait a
+
+f4 :: IO ()
+f4 = forever $ do
+  t1 <- forkIO f1
+  throwTo t1 (userError "aa")
